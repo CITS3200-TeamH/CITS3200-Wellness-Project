c50d7759c016914dc3059f026241dd798e10daaa
Ruvan Muthu-Krishna <ruvan@ozemail.com.au>
2011-09-20 14:33:40 +0800

    playing with encoding token

--- ediary_culled/api_auth.php
+++ ediary_culled/api_auth.php
@@ -41,8 +41,8 @@
               $token = $token . "+" . (string) $id;
             // should encrypt the string here.  Possibly use convert_uuencode($token);
               
-       // $encoded = convert_uuencode($token);
-        $encoded = $token;
+        $encoded = convert_uuencode($token);
+        echo "<br> token=" . $token;
         
         $reply = new SimpleXMLElement("<reply></reply>");
         $reply->addAttribute('token', $encoded);
