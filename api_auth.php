8ae35f219bfb114a6e4e0ac5b6e604f7ec0ba2ef
Ruvan Muthu-Krishna <ruvan@ozemail.com.au>
2011-09-20 10:20:17 +0800

    added api_auth
    api_auth.php will receive a username and password pair which if valid
    it returns a corresponding token for.
    It also has the function validateToken($token) which will return the
    userid if the token is valid and an error message otherwise.

---
+++ api_auth.php
@@ -0,0 +1,55 @@
+<?php
+    
+    $tbl_name = "student";
+	
+    include "config.php";
+    include "layout.php";
+	include "connect.php";
+    
+	if (isset($_POST["username"], $_POST["password"])) {
+		$username = escape_data($_POST["username"]);
+		$password = escape_data($_POST["password"]);
+		
+		if (is_int_val($username)) {
+			$sql="SELECT * FROM $tbl_name, classmap WHERE student.id='$username' AND student.password='$password' AND classmap.id=student_id";
+			$result = mysql_query($sql);
+            
+			if (mysql_num_rows($result) != 0) {
+				//generate token
+                $row=mysql_fetch_array($result);
+                $token = generateToken($row['id']); // Should really check if student is active (student.active bool in db)
+                
+                
+                
+			} else {
+				echo "NO";
+			}
+		} else {
+			echo "NO";
+		}
+	} 
+    
+    //elseif (isset($_POST["token"]) {
+    //    $token = escape_data($_POST["token"]);
+    //    validateToken($token);   
+    //         
+    //}
+
+    
+    // I think we're going to have to make an assumption about the length of the users id, I think if they use UWA's id's 8 should be fine.
+    function generateToken($id) {          
+              $token = (string) idate("U");
+              $token = $token . "+" . (string) $id;
+            // should encrypt the string here.  Possibly use convert_uuencode($token);
+              return $token;
+    }
+
+    function validateToken($token) {
+        // should decrypt the string here. Possibly use convert_uudecode($token);
+        $time = (int) substr($token,0,strpos($token,"+"));
+        
+        if((idate("U")-$time)<86400) {
+        $id = (int) substr($token,strpos($token,"+"));
+        
+        
+    }
