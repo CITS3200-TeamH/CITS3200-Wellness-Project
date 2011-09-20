091ee60805728bc10521711480a817e292a0ca68
Ruvan Muthu-Krishna <ruvan@ozemail.com.au>
2011-09-20 17:33:37 +0800

    query working

--- ediary_culled/api_auth.php
+++ ediary_culled/api_auth.php
@@ -15,7 +15,7 @@
         echo "vars set";
         
 		if (is_int_val($username)) {
-			$sql="SELECT * FROM $tbl_name WHERE binary(id)=$username";
+			$sql="SELECT * FROM $tbl_name WHERE binary(id)='$username' AND binary(password)='$password' AND id=student_id";
 			$result = mysql_query($sql);
                 echo "query executed";
         
