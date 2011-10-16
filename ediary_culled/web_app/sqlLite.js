398ab659e277d7576a0388aa46570bddd498cf7c
CokaRoach <roche.liam.email+github@gmail.com>
2011-10-16 17:59:07 +0800

    testing: minor change

--- ediary_culled/web_app/sqlLite.js
+++ ediary_culled/web_app/sqlLite.js
@@ -699,22 +699,22 @@ function createNewFitnessTest(){
 				if(r.rows.length==1){
 					var studentid = r.rows.item(0)['id'];
 					t.executeSql('Update Student Set time=? Where id=?', [currentTime.getTime()+900000,studentid], function (t, r) {},function (t, error) {alert('Obtaining Rating Items Error: '+error.message+' (Code '+error.code+')');;});
-					tx.executeSql('Select * From class Where start<? And finish>? And (Select count(student_id) From classmap Where student_id = ? And class_name=class.name)>0',[currentTime.getTime(),currentTime.getTime(),studentid],function (t, r) {
-					if(r.rows.length==1){
-						alert();
-						var classname = r.rows.item(0)['name'];
-						t.executeSql('Insert Into Fitness_Test(student_id,group_id,daydate,pushup,situp,chinup,hang,sitreach1,sitreach2,height,mass,waist,hip,uploaded) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[studentid,classname,daydate.getTime(),pushup,situp,chinup,hang,sitreach1,sitreach2,height,mass,waist,hip,false],
-							function (t, r) {
-								document.location = "Fitness.html";
-							},function (t, error) {alert('Error: '+error.message+' (Code '+error.code+')');;});
-						alert();
-					} else {
-						alert("You are currently enrolled in multiple classes or no classes. Sorry this application cannot handle this event.");
-					}
-				},function (t, error) {alert('Obtaining Class Error: '+error.message+' (Code '+error.code+')');;});
-					} else {
-						document.location = "LoginTimedOut.html";
-					}
+					t.executeSql('Select * From class Where start<? And finish>? And (Select count(student_id) From classmap Where student_id = ? And class_name=class.name)>0',[currentTime.getTime(),currentTime.getTime(),studentid],function (t, r) {
+						if(r.rows.length==1){
+							//alert();
+							var classname = r.rows.item(0)['name'];
+							t.executeSql('Insert Into Fitness_Test(student_id,group_id,daydate,pushup,situp,chinup,hang,sitreach1,sitreach2,height,mass,waist,hip,uploaded) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[studentid,classname,daydate.getTime(),pushup,situp,chinup,hang,sitreach1,sitreach2,height,mass,waist,hip,false],
+								function (t, r) {
+									document.location = "Fitness.html";
+								},function (t, error) {alert('Error: '+error.message+' (Code '+error.code+')');;});
+							//alert();
+						} else {
+							alert("You are currently enrolled in multiple classes or no classes. Sorry this application cannot handle this event.");
+						}
+					},function (t, error) {alert('Obtaining Class Error: '+error.message+' (Code '+error.code+')');;});
+				} else {
+					document.location = "LoginTimedOut.html";
+				}
 			},function (t, error) {alert('Error: '+error.message+' (Code '+error.code+')');;});
 		});
 	}
